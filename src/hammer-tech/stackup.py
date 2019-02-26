eebdb561beaa266112e78bd50aaf1156e7b7492d
Edward Wang <edward.c.wang@compdigitec.com>
2019-02-26 01:28:26 -0800

    Pydoc and style fixes

--- src/hammer-tech/stackup.py
+++ src/hammer-tech/stackup.py
@@ -30,6 +30,7 @@ class RoutingDirection(Enum):
 
     @staticmethod
     def from_str(input_str: str) -> "RoutingDirection":
+        # pylint: disable=missing-docstring
         try:
             return RoutingDirection.__mapping()[input_str]
         except KeyError:
@@ -39,6 +40,11 @@ class RoutingDirection(Enum):
         return reverse_dict(RoutingDirection.__mapping())[self]
 
     def opposite(self) -> "RoutingDirection":
+        """
+        Return the opposite routing direction.
+        For Redistribution, this returns itself.
+        :return: Opposite routing direction
+        """
         if self == RoutingDirection.Vertical:
             return RoutingDirection.Horizontal
         elif self == RoutingDirection.Horizontal:
@@ -66,6 +72,7 @@ class WidthSpacingTuple(NamedTuple('WidthSpacingTuple', [
 
     @staticmethod
     def from_setting(d: dict) -> "WidthSpacingTuple":
+        # pylint: disable=missing-docstring
         width_at_least = float(d["width_at_least"])
         min_spacing = float(d["min_spacing"])
         assert width_at_least >= 0.0
@@ -77,11 +84,13 @@ class WidthSpacingTuple(NamedTuple('WidthSpacingTuple', [
 
     @staticmethod
     def from_list(l: List[dict]) -> List["WidthSpacingTuple"]:
-        out = sorted(list(map(lambda x: WidthSpacingTuple.from_setting(x), l)), key=lambda x: x.width_at_least)
-        s = 0.0
+        out = sorted(list(map(WidthSpacingTuple.from_setting, l)), key=lambda x: x.width_at_least)
+
+        # Check that spacings increase.
+        current_spacing = 0.0
         for wst in out:
-            assert wst.min_spacing >= s
-            s = wst.min_spacing
+            assert wst.min_spacing >= current_spacing
+            current_spacing = wst.min_spacing
         return out
 
 
@@ -135,6 +144,7 @@ class Metal(NamedTuple('Metal', [
 
     @staticmethod
     def from_setting(d: dict) -> "Metal":
+        # pylint: disable=missing-docstring
         return Metal(
             name=str(d["name"]),
             index=int(d["index"]),
@@ -161,82 +171,136 @@ class Metal(NamedTuple('Metal', [
                 return spacing
         return spacing
 
-    # Derive the minimum spacing for a maximally-sized wire given a desired pitch.
-    # Use this when the wire width is unknown, but you know the pitch.
-    # This calculation essentially plots the wire width on the X axis and the minimum pitch on the Y axis.
-    # You'll see discontinuites at the width-spacing table entries. If the desired pitch falls on a sloped
-    # line (i.e. > min width for entry N but less than min width for entry N+1), pick that spacing. If
-    # the desired pitch falls on a vertical line, pick the maximum width entry for N, which is the
-    # entry for N+1 minus delta (2 grid units), and then the spacing will be larger than the min spacing.
-    def min_spacing_from_pitch(self, pitch: float) -> float:
-        ws = self.power_strap_widths_and_spacings
-        spacing = ws[0].min_spacing
-        for first, second in zip(ws[:-1], ws[1:]):
+    def min_spacing_and_max_width_from_pitch(self, pitch: float) -> Tuple[float, float]:
+        """
+        Derive the minimum spacing and maximally-sized wire for a
+        desired pitch.
+
+        Use this when the wire width is unknown, but you know the pitch.
+        This calculation essentially plots the wire width on the X axis
+        and the minimum pitch on the Y axis.
+
+        You'll see discontinuites at the width-spacing table entries.
+        If the desired pitch falls on a sloped line (i.e. > min width for
+        entry N but less than min width for entry N+1), pick that spacing.
+        If the desired pitch falls on a vertical line, pick the maximum width
+        entry for N, which is the entry for N+1 minus delta (2 grid units),
+        and then the spacing will be larger than the min spacing.
+
+        :param pitch: Desired pitch
+        :return: Tuple of (minimum spacing, maximally-sized wire)
+        """
+        widths_and_spacings = self.power_strap_widths_and_spacings
+        spacing = widths_and_spacings[0].min_spacing
+        for first, second in zip(widths_and_spacings[:-1], widths_and_spacings[1:]):
             if pitch >= (second.min_spacing + second.width_at_least):
                 spacing = second.min_spacing
             elif pitch >= (first.min_spacing + second.width_at_least):
                 # we are asking for a pitch that is width-constrained
                 width = self.snap(second.width_at_least - (self.grid_unit*2))
                 spacing = self.snap(pitch - width)
-        return spacing
 
-    # This method will return the maximum width a wire can be to consume a given number of routing tracks.
-    # This assumes the neighbors of the wide wire are minimum-width routes.
-    # i.e. T W T
-    # T = thin / min-width
-    # W = wide
-    # Returns width, spacing and start
-    # See min_spacing_from_pitch documentation for an explanation of the calculation.
+        width = self.snap(pitch - spacing)
+        if width < 0.0:
+            raise ValueError("Desired pitch {pitch} is illegal".format(pitch=pitch))
+        return spacing, self.snap(pitch - spacing)
+
+    def min_spacing_from_pitch(self, pitch: float) -> float:
+        """
+        Derive the minimum spacing for a maximally-sized wire given a
+        desired pitch.
+        See min_spacing_and_max_width_from_pitch for details.
+
+        :param pitch: Desired pitch
+        :return: Minimum spacing for said pitch.
+        """
+        return self.min_spacing_and_max_width_from_pitch(pitch)[0]
+
+    def max_width_from_pitch(self, pitch: float) -> float:
+        """
+        Derive the maximum wire width for a maximally-sized wire given a
+        desired pitch.
+        See min_spacing_and_max_width_from_pitch for details.
+
+        :param pitch: Desired pitch
+        :return: Maximum wire width for said pitch.
+        """
+        return self.min_spacing_and_max_width_from_pitch(pitch)[1]
+
     def get_width_spacing_start_twt(self, tracks: int) -> Tuple[float, float, float]:
-        ws = self.power_strap_widths_and_spacings
-        spacing = ws[0].min_spacing
+        """
+        This method will return the maximum width a wire can be in order
+        to consume a given number of routing tracks.
+        This assumes the neighbors of the wide wire are minimum-width routes.
+        i.e. T W T
+        T = thin / min-width
+        W = wide
+        See min_spacing_and_max_width_from_pitch for an explanation of the calculation.
+
+        :param tracks: Number of routing tracks to consume
+        :return: Returns tuple of (width, spacing, start)
+        """
+        widths_and_spacings = self.power_strap_widths_and_spacings
+        spacing = widths_and_spacings[0].min_spacing
         # the T W T pattern contains one wires (W) and 2 spaces (S2)
         s2w = self.snap((tracks + 1) * self.pitch - self.min_width)
-        assert (int(self.snap(s2w / self.grid_unit)) % 2 == 0), "This calculation should always produce an even s2w"
+
+        assert int(self.snap(s2w / self.grid_unit)) % 2 == 0, "This calculation should always produce an even s2w"
+
         width = self.snap(s2w - spacing*2)
-        for first, second in zip(ws[:-1], ws[1:]):
-            if s2w >= (second.min_spacing*2 + second.width_at_least):
+        for first, second in zip(widths_and_spacings[:-1], widths_and_spacings[1:]):
+            if s2w >= second.min_spacing*2 + second.width_at_least:
                 spacing = second.min_spacing
                 width = self.snap(s2w - spacing*2)
-            elif s2w >= (first.min_spacing*2 + second.width_at_least):
+            elif s2w >= first.min_spacing*2 + second.width_at_least:
                 # we are asking for a pitch that is width-constrained
-                if (int(second.width_at_least / self.grid_unit) % 2 == 0):
+                if int(second.width_at_least / self.grid_unit) % 2 == 0:
                     # even
                     width = self.snap(second.width_at_least - (self.grid_unit*2))
                 else:
                     # odd
                     width = self.snap(second.width_at_least - self.grid_unit)
                 spacing = self.snap((s2w - width)/2.0)
-        assert (int(self.snap(self.min_width / self.grid_unit)) % 2 == 0), "Assuming all min widths are even here, if not fix me"
-        assert (int(self.snap(width / self.grid_unit)) % 2 == 0), "This calculation should always produce an even width"
-        start = self.snap(self.min_width/2.0 + spacing)
+
+        assert int(self.snap(self.min_width / self.grid_unit)) % 2 == 0, (
+            "Assuming all min widths are even here, if not fix me")
+        assert int(self.snap(width / self.grid_unit)) % 2 == 0, (
+            "This calculation should always produce an even width")
+
+        start = self.snap(self.min_width / 2.0 + spacing)
         return (width, spacing, start)
 
-    # This method will return the maximum width a wire can be to consume a given number of routing tracks
-    # This assumes one neighbor is a min width wire, and the other is the same size as this (mirrored)
-    # i.e. T W W T
-    # T = thin / min-width
-    # W = wide
-    # Returns width, spacing, and start
-    # See min_spacing_from_pitch documentation for an explanation of the calculation
-    # force_even: True to force the strap width to be an even multiple of the grid unit
     def get_width_spacing_start_twwt(self, tracks: int, force_even: bool = False) -> Tuple[float, float, float]:
-        ws = self.power_strap_widths_and_spacings
-        spacing = ws[0].min_spacing
+        """
+        This method will return the maximum width a wire can be in order
+        to consume a given number of routing tracks.
+        This assumes that the wires are in the following configuration.
+        TODO: double check that this pydoc is correct
+        i.e. T W W T
+        T = thin / min-width
+        W = wide
+        See min_spacing_and_max_width_from_pitch for an explanation of the calculation.
+
+        :param tracks: Number of routing tracks to consume
+        :param force_even: TODO document me
+        :return: Returns tuple of (width, spacing, start)
+        """
+        widths_and_spacings = self.power_strap_widths_and_spacings
+        spacing = widths_and_spacings[0].min_spacing
         # the T W W T pattern contains two wires (W2) and 3 spaces (S3)
         s3w2 = self.snap(((2*tracks) + 1) * self.pitch - self.min_width)
         width = self.snap((s3w2 - spacing*3)/2.0)
-        for first, second in zip(ws[:-1], ws[1:]):
-            if s3w2 >= (second.min_spacing*3 + second.width_at_least*2):
+        for first, second in zip(widths_and_spacings[:-1], widths_and_spacings[1:]):
+            if s3w2 >= second.min_spacing*3 + second.width_at_least*2:
                 spacing = second.min_spacing
                 width = self.snap((s3w2 - spacing*3)/2.0)
-            elif s3w2 >= (first.min_spacing*3 + second.width_at_least*2):
+            elif s3w2 >= first.min_spacing*3 + second.width_at_least*2:
                 # we are asking for a pitch that is width-constrained
                 width = self.snap(second.width_at_least - (self.grid_unit*1))
                 spacing = self.snap((s3w2 - width*2)/3.0)
-        assert (int(self.min_width / self.grid_unit) % 2 == 0), "Assuming all min widths are even here, if not fix me"
+        assert int(self.min_width / self.grid_unit) % 2 == 0, "Assuming all min widths are even here, if not fix me"
         start = self.snap(self.min_width/2.0 + spacing)
-        if force_even and (int(width / self.grid_unit) % 2 == 1):
+        if force_even and int(width / self.grid_unit) % 2 == 1:
             width = self.snap(width - self.grid_unit)
             start = self.snap(start + self.grid_unit)
         return (width, spacing, start)
@@ -245,8 +309,8 @@ class Metal(NamedTuple('Metal', [
 
 
 class Stackup(NamedTuple('Stackup', [
-    ('name', str),
-    ('metals', List[Metal])
+        ('name', str),
+        ('metals', List[Metal])
 ])):
     """
     A stackup is a list of metals with a meaningful keyword name (for now).
@@ -257,14 +321,20 @@ class Stackup(NamedTuple('Stackup', [
 
     @staticmethod
     def from_setting(d: dict) -> "Stackup":
+        # pylint: disable=missing-docstring
         return Stackup(
             name=str(d["name"]),
             metals=list(map(lambda x: Metal.from_setting(x), list(d["metals"])))
         )
 
-    def get_metal(self, name: str) -> "Metal":
+    def get_metal(self, name: str) -> Metal:
+        """
+        Get a given metal layer by name.
+
+        :param name: Name of the metal layer
+        :return: A metal layer object
+        """
         for m in self.metals:
             if m.name == name:
                 return m
         raise ValueError("Metal named %s is not defined in stackup %s" % (name, self.name))
-
